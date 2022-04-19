2cfb3e2722ababc271590a1acfd781a58cdc0459
Andrew Lai <arjlai221@gmail.com>
2022-04-18 17:06:11 -0700

    first part working

--- program1_lwp/lwp.c
+++ program1_lwp/lwp.c
@@ -3,32 +3,39 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+int roundRobinScheduling();
+int getScheduledThread();
+
 schedfun schedulingFunction = NULL;
 int* mainSP = NULL;
 
+lwp_context lwp_ptable[LWP_PROC_LIMIT];
+int lwp_procs = 0;
+int lwp_running = -1;
+
 int new_lwp(lwpfun functionPointer, void * argument, size_t words){
-    int *stack = (int*) malloc(4 * words);
-    int *sp = stack + words;
+    ptr_int_t *stack = (ptr_int_t*) malloc(sizeof(ptr_int_t) * words);
+    ptr_int_t *sp = stack + words + 1;
 
     // push the argument to the stack
     sp -= 1;
-    *sp = (intptr_t) argument;
+    *sp = (ptr_int_t) argument;
 
     // push return address to stack
     sp -= 1;
     // TODO: change -1 to be &lwp_exit
-    *sp = -1;
+    *sp = (ptr_int_t) lwp_exit;
 
     // push functionPointer to stack
     sp -= 1;
     // ASK PROFESSOR ABOUT THIS CAST
-    *sp = (intptr_t) functionPointer;
+    *sp = (ptr_int_t) functionPointer;
 
     // push bogus base pointer to stack
     sp -= 1;
-    *sp = -1;
+    *sp = 999;
 
-    int* bbpAddr = sp;
+    ptr_int_t *bbpAddr = sp;
 
     sp -= 6;
     *sp = 0x6666;
@@ -38,18 +45,14 @@ int new_lwp(lwpfun functionPointer, void * argument, size_t words){
     *(sp + 4) = 0x2222;
     *(sp + 5) = 0x1111;
 
-    sp -= 7;
-    *sp = (intptr_t) bbpAddr;
-
-    if(lwp_procs == 0){
-        //lwp_ptable = (lwp_context*) malloc(sizeof(lwp_context) * 256);
-    }
+    sp -= 1;
+    *sp = (ptr_int_t) bbpAddr;
 
     // create new context
     lwp_context newContext;
     newContext.pid = lwp_procs;
-    newContext.sp = (ptr_int_t*) sp;
-    newContext.stack = (ptr_int_t*) stack;
+    newContext.sp = sp;
+    newContext.stack = stack;
     // stacksize is number of words in stack not byte size of stack
     newContext.stacksize = words;
 
@@ -84,23 +87,11 @@ void lwp_start(){
     // save current thread's context
     SAVE_STATE();
 
-    // current thread is not main thread (may not need this check)
-    if(lwp_running != -1){
-        GetSP(lwp_ptable[lwp_running].sp);
-    }
-    // current thread is main thread
-    else{
-        GetSP(mainSP);
-    }
+    GetSP(mainSP);
 
     // select thread to run
-    int threadToRun;
-    if(schedulingFunction == NULL){
-        threadToRun = (lwp_running + 1)%lwp_procs; 
-    }
-    else{
-        threadToRun = schedulingFunction();
-    }
+    // int threadToRun = getScheduledThread();
+    int threadToRun = 0;
 
     // set stack pointer to thread's stack pointer
     SetSP(lwp_ptable[threadToRun].sp);
@@ -112,6 +103,48 @@ void lwp_start(){
     return;
 }
 
+void lwp_yield(){
+    SAVE_STATE();
+
+    GetSP(lwp_ptable[lwp_running].sp);
+
+    // select thread to run
+    int threadToRun = getScheduledThread();
+
+        // set stack pointer to thread's stack pointer
+    SetSP(lwp_ptable[threadToRun].sp);
+    lwp_running = threadToRun;
+    // load new thread's state
+    RESTORE_STATE();
+
+    // return (using the trick to jump into thread's function)
+    return;
+}
+
+int getScheduledThread(){
+    if(schedulingFunction == NULL){
+        // change this to round robin (each one gets equal time)
+        return roundRobinScheduling();
+    }
+    else{
+        return schedulingFunction();
+    }
+}
+
+int roundRobinScheduling(){
+    return 1;
+}
+
 void lwp_set_scheduler(schedfun sched){
     schedulingFunction = sched;
+}
+
+void lwp_exit(){
+    // delete the current running entry in lwp_ptable
+    // move subsequent entries to one lower
+    // decrement lwp_procs
+    // set current running thread to -1
+    // set SP to main thread's SP
+    // restore state
+    // return?
 }
--- program1_lwp/lwp.s
+++ program1_lwp/lwp.s
@@ -13,6 +13,20 @@ schedulingFunction:
 	.size	mainSP, 8
 mainSP:
 	.zero	8
+	.comm	lwp_ptable,960,32
+	.globl	lwp_procs
+	.align 4
+	.type	lwp_procs, @object
+	.size	lwp_procs, 4
+lwp_procs:
+	.zero	4
+	.globl	lwp_running
+	.data
+	.align 4
+	.type	lwp_running, @object
+	.size	lwp_running, 4
+lwp_running:
+	.long	-1
 	.text
 	.globl	new_lwp
 	.type	new_lwp, @function
@@ -30,56 +44,55 @@ new_lwp:
 	movq	%rsi, -80(%rbp)
 	movq	%rdx, -88(%rbp)
 	movq	-88(%rbp), %rax
-	salq	$2, %rax
+	salq	$3, %rax
 	movq	%rax, %rdi
 	call	malloc@PLT
 	movq	%rax, -56(%rbp)
 	movq	-88(%rbp), %rax
-	leaq	0(,%rax,4), %rdx
+	addq	$1, %rax
+	leaq	0(,%rax,8), %rdx
 	movq	-56(%rbp), %rax
 	addq	%rdx, %rax
 	movq	%rax, -48(%rbp)
-	subq	$4, -48(%rbp)
-	movq	-80(%rbp), %rax
-	movl	%eax, %edx
+	subq	$8, -48(%rbp)
+	movq	-80(%rbp), %rdx
 	movq	-48(%rbp), %rax
-	movl	%edx, (%rax)
-	subq	$4, -48(%rbp)
+	movq	%rdx, (%rax)
+	subq	$8, -48(%rbp)
+	leaq	lwp_exit(%rip), %rdx
 	movq	-48(%rbp), %rax
-	movl	$-1, (%rax)
-	subq	$4, -48(%rbp)
-	movq	-72(%rbp), %rax
-	movl	%eax, %edx
+	movq	%rdx, (%rax)
+	subq	$8, -48(%rbp)
+	movq	-72(%rbp), %rdx
 	movq	-48(%rbp), %rax
-	movl	%edx, (%rax)
-	subq	$4, -48(%rbp)
+	movq	%rdx, (%rax)
+	subq	$8, -48(%rbp)
 	movq	-48(%rbp), %rax
-	movl	$-1, (%rax)
+	movq	$999, (%rax)
 	movq	-48(%rbp), %rax
 	movq	%rax, -40(%rbp)
-	subq	$24, -48(%rbp)
+	subq	$48, -48(%rbp)
 	movq	-48(%rbp), %rax
-	movl	$26214, (%rax)
-	movq	-48(%rbp), %rax
-	addq	$4, %rax
-	movl	$21845, (%rax)
+	movq	$26214, (%rax)
 	movq	-48(%rbp), %rax
 	addq	$8, %rax
-	movl	$17476, (%rax)
-	movq	-48(%rbp), %rax
-	addq	$12, %rax
-	movl	$13107, (%rax)
+	movq	$21845, (%rax)
 	movq	-48(%rbp), %rax
 	addq	$16, %rax
-	movl	$8738, (%rax)
+	movq	$17476, (%rax)
 	movq	-48(%rbp), %rax
-	addq	$20, %rax
-	movl	$4369, (%rax)
-	subq	$28, -48(%rbp)
-	movq	-40(%rbp), %rax
-	movl	%eax, %edx
+	addq	$24, %rax
+	movq	$13107, (%rax)
 	movq	-48(%rbp), %rax
-	movl	%edx, (%rax)
+	addq	$32, %rax
+	movq	$8738, (%rax)
+	movq	-48(%rbp), %rax
+	addq	$40, %rax
+	movq	$4369, (%rax)
+	subq	$8, -48(%rbp)
+	movq	-40(%rbp), %rdx
+	movq	-48(%rbp), %rax
+	movq	%rdx, (%rax)
 	movl	lwp_procs(%rip), %eax
 	cltq
 	movq	%rax, -32(%rbp)
@@ -123,63 +136,196 @@ lwp_start:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	subq	$16, %rsp
 	movl	lwp_procs(%rip), %eax
 	testl	%eax, %eax
-	je	.L10
+	je	.L6
 #APP
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %rax
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %rbx
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %rcx
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %rdx
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %rsi
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %rdi
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %r8
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %r9
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %r10
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %r11
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %r12
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %r13
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %r14
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
 	pushq %r15
 # 0 "" 2
-# 86 "lwp.c" 1
+# 88 "lwp.c" 1
+	pushq %rbp
+# 0 "" 2
+# 90 "lwp.c" 1
+	movq  %rsp,%rax
+# 0 "" 2
+#NO_APP
+	movq	%rax, mainSP(%rip)
+	movl	$0, -4(%rbp)
+	movl	-4(%rbp), %eax
+	cltq
+	salq	$5, %rax
+	movq	%rax, %rdx
+	leaq	24+lwp_ptable(%rip), %rax
+	movq	(%rdx,%rax), %rax
+#APP
+# 97 "lwp.c" 1
+	movq  %rax,%rsp
+# 0 "" 2
+#NO_APP
+	movl	-4(%rbp), %eax
+	movl	%eax, lwp_running(%rip)
+#APP
+# 100 "lwp.c" 1
+	popq  %rbp
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %r15
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %r14
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %r13
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %r12
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %r11
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %r10
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %r9
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %r8
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %rdi
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %rsi
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %rdx
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %rcx
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %rbx
+# 0 "" 2
+# 100 "lwp.c" 1
+	popq  %rax
+# 0 "" 2
+# 100 "lwp.c" 1
+	movq  %rbp,%rsp
+# 0 "" 2
+#NO_APP
+	jmp	.L3
+.L6:
+	nop
+.L3:
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE7:
+	.size	lwp_start, .-lwp_start
+	.globl	lwp_yield
+	.type	lwp_yield, @function
+lwp_yield:
+.LFB8:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+#APP
+# 107 "lwp.c" 1
+	pushq %rax
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %rbx
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %rcx
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %rdx
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %rsi
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %rdi
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %r8
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %r9
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %r10
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %r11
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %r12
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %r13
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %r14
+# 0 "" 2
+# 107 "lwp.c" 1
+	pushq %r15
+# 0 "" 2
+# 107 "lwp.c" 1
 	pushq %rbp
 # 0 "" 2
 #NO_APP
-	movl	lwp_running(%rip), %eax
-	cmpl	$-1, %eax
-	je	.L6
 	movl	lwp_running(%rip), %edx
 #APP
-# 90 "lwp.c" 1
+# 109 "lwp.c" 1
 	movq  %rsp,%rax
 # 0 "" 2
 #NO_APP
@@ -188,30 +334,9 @@ lwp_start:
 	salq	$5, %rcx
 	leaq	24+lwp_ptable(%rip), %rdx
 	movq	%rax, (%rcx,%rdx)
-	jmp	.L7
-.L6:
-#APP
-# 94 "lwp.c" 1
-	movq  %rsp,%rax
-# 0 "" 2
-#NO_APP
-	movq	%rax, mainSP(%rip)
-.L7:
-	movq	schedulingFunction(%rip), %rax
-	testq	%rax, %rax
-	jne	.L8
-	movl	lwp_running(%rip), %eax
-	addl	$1, %eax
-	movl	lwp_procs(%rip), %ecx
-	cltd
-	idivl	%ecx
-	movl	%edx, -4(%rbp)
-	jmp	.L9
-.L8:
-	movq	schedulingFunction(%rip), %rax
-	call	*%rax
+	movl	$0, %eax
+	call	getScheduledThread
 	movl	%eax, -4(%rbp)
-.L9:
 	movl	-4(%rbp), %eax
 	cltq
 	salq	$5, %rax
@@ -219,72 +344,153 @@ lwp_start:
 	leaq	24+lwp_ptable(%rip), %rax
 	movq	(%rdx,%rax), %rax
 #APP
-# 107 "lwp.c" 1
+# 115 "lwp.c" 1
 	movq  %rax,%rsp
 # 0 "" 2
 #NO_APP
 	movl	-4(%rbp), %eax
 	movl	%eax, lwp_running(%rip)
 #APP
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %rbp
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %r15
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %r14
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %r13
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %r12
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %r11
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %r10
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %r9
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %r8
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %rdi
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %rsi
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %rdx
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %rcx
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %rbx
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	popq  %rax
 # 0 "" 2
-# 110 "lwp.c" 1
+# 118 "lwp.c" 1
 	movq  %rbp,%rsp
 # 0 "" 2
 #NO_APP
-	jmp	.L3
-.L10:
 	nop
-.L3:
 	leave
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE7:
-	.size	lwp_start, .-lwp_start
+.LFE8:
+	.size	lwp_yield, .-lwp_yield
+	.globl	getScheduledThread
+	.type	getScheduledThread, @function
+getScheduledThread:
+.LFB9:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	schedulingFunction(%rip), %rax
+	testq	%rax, %rax
+	jne	.L10
+	movl	$0, %eax
+	call	roundRobinScheduling
+	jmp	.L11
+.L10:
+	movq	schedulingFunction(%rip), %rax
+	call	*%rax
+.L11:
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE9:
+	.size	getScheduledThread, .-getScheduledThread
+	.globl	roundRobinScheduling
+	.type	roundRobinScheduling, @function
+roundRobinScheduling:
+.LFB10:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$1, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE10:
+	.size	roundRobinScheduling, .-roundRobinScheduling
+	.globl	lwp_set_scheduler
+	.type	lwp_set_scheduler, @function
+lwp_set_scheduler:
+.LFB11:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -8(%rbp)
+	movq	-8(%rbp), %rax
+	movq	%rax, schedulingFunction(%rip)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE11:
+	.size	lwp_set_scheduler, .-lwp_set_scheduler
+	.globl	lwp_exit
+	.type	lwp_exit, @function
+lwp_exit:
+.LFB12:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE12:
+	.size	lwp_exit, .-lwp_exit
 	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
