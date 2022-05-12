1cf54b0a080c33c8c21da36cb51f8a67ac295bff
Andrew Lai <arjlai221@gmail.com>
2022-05-12 13:14:34 -0700

    starting memory simulator

---
+++ program3_memory/memClasses.py
@@ -0,0 +1,28 @@
+from numpy import int16
+
+
+class TLBEntry():
+    def __init__(self, pageNum : int, frameNum : int) -> None:
+        self.pageNum = pageNum
+        self.frameNum = frameNum
+
+class TLB():
+    def __init__(self) -> None:
+        self.entries : list[TLBEntry] = [None] * 16 
+
+    def inTLB(pageNum : int) -> bool:
+        # searches for pageNum in self.entries
+        pass
+
+class PageTableEntry():
+    def __init__(self, frameNum : int):
+        self.frameNum = frameNum
+        self.valid = False
+        
+class PageTable():
+    def __init__(self):
+        self.entries : list[PageTableEntry] = [None] * (2 ** 8)
+
+class RAM():
+    def __init__(self, frames:int):
+        self.frames : list[bytes]= [None] * frames
