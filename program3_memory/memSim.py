089719b8ba6135b2ca2c4c4b6aa23a88d49ce379
Andrew Lai <arjlai221@gmail.com>
2022-05-31 09:53:57 -0700

    updating to Desktop version

--- program3_memory/memSim.py
+++ program3_memory/memSim.py
@@ -244,7 +244,6 @@ class MemSimulator():
     
     def twos_comp(self, val, bits = 8):
         """compute the 2's complement of int value val"""
-        print(bits)
         if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
             val = val - (1 << bits)        # compute negative value
         return val                         # return positive value as is
