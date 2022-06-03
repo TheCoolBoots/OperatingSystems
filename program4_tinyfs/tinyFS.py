a15f06d5e08a5ed1f80e5d6aeb0be1dcb658216d
Andrew Lai <arjlai221@gmail.com>
2022-06-02 18:46:25 -0700

    write3 working

--- program4_tinyfs/tinyFS.py
+++ program4_tinyfs/tinyFS.py
@@ -119,6 +119,7 @@ def tfs_write(FD:fileDescriptor, writeBuffer:buffer, size:int):
             i += 1
         for j in range(0, blocksNeeded - blocksAllocated):
             fileINode.dataBlockPtrs[i+j] = cmd.getNextFreeBlockIndex()
+            # TODO make sure the inode is getting updated in the DRT
 
 
     # freeBytes in current block = 256 - (fileINode.filePointer % 256)
@@ -170,7 +171,7 @@ def tfs_write(FD:fileDescriptor, writeBuffer:buffer, size:int):
     if bytesToWrite > 0:
         # write the remaining bytes in values to the next data block
         dsk.readBlock(cmdid, fileINode.dataBlockPtrs[dataBlockIndex], b)
-        newContents = writeBuffer.contents[:valuePtr] + b.contents[bytesToWrite:]
+        newContents = writeBuffer.contents[valuePtr:] + b.contents[bytesToWrite:]
         dsk.writeBlock(cmdid, fileINode.dataBlockPtrs[dataBlockIndex], buffer(newContents))
 
     if fileINode.filePointer > fileINode.filesize:
